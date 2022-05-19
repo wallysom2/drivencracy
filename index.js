f86406d8ce16095ce4dd2ca3717762e120ec2448
Wallyson <wallysom2@gmail.com>
2022-05-19 11:24:27 -0300

    Criando opções para as enquetes

--- index.js
+++ index.js
@@ -42,9 +42,35 @@ app.get("/poll", async (req, res) => {
     }
 });
 
+// Criando uma opção
+app.post("/choice", async (req, res) => {
+    const { title,pollId} = req.body;
+    //validação joi
+    const schema = joi.object({
+        title: joi.string().required(),
+        pollId: joi.string().required()       
+        })
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) return res.status(422).send(error.details[0].message);
+
+    // somente criar uma opção se a poll existir
+    try {
+        const poll = await db.collection("polls").findOne({ _id: pollId });
+        if (!poll) return res.status(404).send("Poll not found");
+        await db.collection("choices").insertOne({
+            title,
+            pollId
+        });
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+});
 
 
-const port = process.env.PORT || 3002;
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
